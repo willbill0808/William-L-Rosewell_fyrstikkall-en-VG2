9f28d6b19c02d0cf79d147df358cbb079829c0b3
William Laugerud Rosewell <wiroa002@William-Laugerud-Rosewells-Mac.local>
2025-10-09 10:53:21 +0200

    mac backup

--- sql_øve_oppgaver/sql_py_øve/butikk.py
+++ sql_øve_oppgaver/sql_py_øve/butikk.py
@@ -13,20 +13,57 @@ CREATE TABLE IF NOT EXISTS inventar (
 )
 """)
 
+c.execute("""
+CREATE TABLE IF NOT EXISTS salg (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    vare_id TEXT NOT NULL,
+    dato REAL,
+    antall INTEGER NOT NULL
+)
+""")
+
+
 def legg_til_vare():
     tittel  = input()
     pris  = input()
     antall  = input()
     c.execute("INSERT INTO inventar (tittel, pris, antall) VALUES (?,?,?)", (tittel, pris, antall))
-    kobling.commit
+    kobling.commit()
+
+
+def salg():
+    c.execute ("SELECT * FROM inventar")
+    rows = c.fetchall()
+
+    for row in rows:
+        print(row)
+
+    print("Hvilken vare vil du selge")
+    want = input(":")
+
+    c.execute(f"SELECT * FROM inventar WHERE tittel LIKE '%{want}%'")
+
+    rows = c.fetchall()
+
+    for row in rows:
+        print(row)
+
+
+
+
 
 inn = ""
 while inn != "q":
     print("""
 MENY
 1. Legg til vare
+2. sell en vare
 q  Avslutt
+
     """)
     inn = input(": ")
     if inn == "1":
         legg_til_vare()
+
+    if inn == "2":
+        salg()
