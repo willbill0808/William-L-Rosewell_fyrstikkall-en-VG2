9db0d491d3b681a0de9013dc5b86813568aeb280
willbill0808 <williamlrosewell08@gmail.com>
2025-10-28 10:17:03 +0100

    colorama

---
+++ sql_øve_oppgaver/sql_py_øve/colour.py
@@ -0,0 +1,6 @@
+from colorama import Fore, Back, Style
+
+print(Fore.RED + 'This text is red.')
+print(Back.GREEN + 'This has a green background.')
+print(Style.BRIGHT + 'This text is bright.')
+print(Fore.BLUE + Back.YELLOW + Style.BRIGHT + 'Blue text, yellow background, bright style.')
